c05d39cf82da4d65fe0c3506c017bd6f7ebaf30c
huanqing19901001 <huanqing19901001@163.com>
2020-11-15 20:45:29 +0800

    开篇

---
+++ chapter01/case01_taste.sql
@@ -0,0 +1,19 @@
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("研发", "深圳", "10000");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("运维", "深圳", "10001");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("人资", "深圳", "10008");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("测试", "深圳", "10002");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("销售", "上海", "10006");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("财务", "深圳", "10003");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("售后", "北京", "10004");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("科技", "深圳", "10005");
+
+insert into mysqlfull.dept (dept_name, address, dept_no) values ("实施", "上海", "10007");
+
+select * from mysqlfull.dept;
